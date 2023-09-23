f381e724ff3f17b55c5d70e669f6c91ddc69dfc6
unnath123 <64188873+unnath123@users.noreply.github.com>
2023-09-24 01:55:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,82 @@
 // complete the js code
 class CustomPromise {
-  // write your code here
-	let promise = new Promise((resolve,reject)=>{
-		resolve("resolved")
-	})
+  constructor() {
+    this.state = 'pending';
+    this.value = undefined;
+    this.reason = undefined;
+    this.onFulfilledCallbacks = [];
+    this.onRejectedCallbacks = [];
+    this.onFinallyCallback = null;
+  }
 
-	promise
-	.then((data)=>{
-		console.log(data)
-	})
-	.catch((e)=>{
-		console.log(e)
-	
-	.finally(()=>{
-		consoel.log("finall")
-	})
+  resolve(value) {
+    if (this.state === 'pending') {
+      this.state = 'fulfilled';
+      this.value = value;
+      this.onFulfilledCallbacks.forEach(callback => callback(this.value));
+      if (this.onFinallyCallback) {
+        this.onFinallyCallback();
+      }
+    }
+  }
+
+  reject(reason) {
+    if (this.state === 'pending') {
+      this.state = 'rejected';
+      this.reason = reason;
+      this.onRejectedCallbacks.forEach(callback => callback(this.reason));
+      if (this.onFinallyCallback) {
+        this.onFinallyCallback();
+      }
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    if (this.state === 'fulfilled') {
+      onFulfilled(this.value);
+    } else if (this.state === 'rejected') {
+      onRejected(this.reason);
+    } else {
+      this.onFulfilledCallbacks.push(onFulfilled);
+      this.onRejectedCallbacks.push(onRejected);
+    }
+    return this;
+  }
+
+  catch(onRejected) {
+    return this.then(null, onRejected);
+  }
+
+  finally(onFinally) {
+    this.onFinallyCallback = onFinally;
+    return this;
+  }
 }
+
+// Example usage:
+const customPromise = new CustomPromise();
+
+customPromise
+  .then(value => {
+    console.log('Fulfilled:', value);
+  })
+  .catch(reason => {
+    console.log('Rejected:', reason);
+  })
+  .finally(() => {
+    console.log('Finally block executed.');
+  });
+
+customPromise.resolve('Hello, world!');
+// Output:
+// Fulfilled: Hello, world!
+// Finally block executed.
+
+// Or in case of rejection:
+// customPromise.reject('Something went wrong.');
+// Output:
+// Rejected: Something went wrong.
+// Finally block executed.
+
 
 window.CustomPromise = CustomPromise;
