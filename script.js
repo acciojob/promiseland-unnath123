4a7f013ff12efeb056055650940f839c38f56e59
unnath123 <64188873+unnath123@users.noreply.github.com>
2023-09-24 01:51:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // complete the js code
 class CustomPromise {
   // write your code here
+	let promise = new Promise((resolve,reject)=>{
+		resolve("resolved")
+	})
+
+	promise
+	.then((data)=>{
+		console.log(data)
+	})
+	.catch((e)=>{
+		console.log(e)
+	
+	.finally(()=>{
+		consoel.log("finall")
+	})
 }
 
 window.CustomPromise = CustomPromise;
